0328dbcf7b314f9a7a0dcaa0e72eb8e7bb5dd9d8
KJ <kernel1983@gmail.com>
2023-04-05 22:22:07 +0800

    test block number and timestamp

--- contract/contracts/pow.sol
+++ contract/contracts/pow.sol
@@ -5,4 +5,16 @@ pragma solidity ^0.8.17;
 contract PoW {
     constructor() {
     }
+
+    function get_number() public view returns (uint) {
+        return block.number;
+    }
+
+    function get_timestamp() public view returns (uint) {
+        return block.timestamp;
+    }
+
+    function get_difficulty(uint8 d) public view returns (uint256) {
+        return 2**d;
+    }
 }
