c7fdb24a5bdd6278eae38eeb117339ab40a3da98
KJ <kernel1983@gmail.com>
2023-04-06 11:53:28 +0800

    mining client received msg

--- backend/server.py
+++ backend/server.py
@@ -29,23 +29,31 @@ class DashboardHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-tasks_pool = []
+# tasks_pool = []
 class ReqHandler(tornado.web.RequestHandler):
     def post(self):
-        global tasks_pool
+        # global tasks_pool
         print(self.request.body)
+        for handler in PoolHandler.miners:
+            handler.write_message(self.request.body)
         self.finish({})
 
 
 class PoolHandler(tornado.websocket.WebSocketHandler):
+    miners = set()
+
     def check_origin(self, origin):
         return True
 
     def open(self):
-        pass
+        if self not in PoolHandler.miners:
+            PoolHandler.miners.add(self)
+            print('self added')
 
     def on_close(self):
-        pass
+        if self in PoolHandler.miners:
+            PoolHandler.miners.remove(self)
+            print('self removed')
 
     # @tornado.gen.coroutine
     def on_message(self, message):
