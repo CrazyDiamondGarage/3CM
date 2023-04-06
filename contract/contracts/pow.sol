9aff934a513a068cec66e2f0cafe7ac39670e3c9
KJ <kernel1983@gmail.com>
2023-04-06 17:52:06 +0800

    update new_block

--- contract/contracts/pow.sol
+++ contract/contracts/pow.sol
@@ -33,25 +33,38 @@ contract PoW {
     uint public prev_timestamp;
     uint public gap_timestamp;
 
-    address public owner;
+    // address public owner;
     uint public block_height;
+    PoWCanidate[] public canidates;
+
+    event NewBlock(address miner, uint height);
 
     constructor() {
-        owner = msg.sender;
+        // owner = msg.sender;
         parent_difficulty = 238;
         parent_height = 0;
-        prev_timestamp = block.timestamp;
+        // prev_timestamp = block.timestamp;
+        prev_timestamp = 0;
         gap_timestamp = 10;
     }
 
     function new_block(bytes32 commitment, address miner, uint64 nonce, UserAction[] memory updates) external returns (bytes32) {
-        // require(block.timestamp >= prev_timestamp + gap_timestamp, "too early");
+        require(block.timestamp >= prev_timestamp + gap_timestamp, "too early");
+        PoWCanidate memory canidate;
+        for (uint i = 0; i < canidates.length; i++) {
+            canidate = canidates[i];
+        }
+
+        console.logBytes32(canidate.commitment);
         // require(parent_height + 1 == mining.block_height, "try add_block");
         bytes32 block_hash = sha256(bytes.concat(parent_hash, commitment, bytes4(parent_height+1), bytes20(miner), bytes8(nonce)));
-        console.logBytes32(parent_hash);
-        console.logUint(uint256(block_hash));
-        console.logUint(2**parent_difficulty);
-        console.logBool(uint256(block_hash) < 2**parent_difficulty);
+        // console.logBytes32(parent_hash);
+        // console.logUint(uint256(block_hash));
+        // console.logUint(2**parent_difficulty);
+        console.logUint(block.timestamp);
+        console.logUint(prev_timestamp);
+        console.logUint(gap_timestamp);
+        // console.logBool(uint256(block_hash) < 2**parent_difficulty);
         require(uint256(block_hash) < 2**parent_difficulty, "difficulty required");
 
         parent_hash = block_hash;
@@ -61,15 +74,15 @@ contract PoW {
         return block_hash;
     }
 
-    function add_block(bytes32 commitment, PoWVote memory mining, UserAction[] memory updates) external returns (bytes32) {
+    function add_block(bytes32 commitment, address miner, uint64 nonce, UserAction[] memory updates) external returns (bytes32) {
         require(parent_height > 0, "try new_block");
-        require(mining.nonce < parent_nonce, "lucky nonce required");
+        // require(nonce < parent_nonce, "lucky nonce required");
         // require(parent_height == mining.block_height, "try new_block");
-        bytes32 block_hash = sha256(bytes.concat(parent_hash, bytes4(parent_height), bytes20(mining.miner), bytes8(mining.nonce)));
+        bytes32 block_hash = sha256(bytes.concat(parent_hash, bytes4(parent_height), bytes20(miner), bytes8(nonce)));
         require(uint256(block_hash) < 2**parent_difficulty, "difficulty required");
 
         parent_hash = block_hash;
-        parent_nonce = mining.nonce;
+        parent_nonce = nonce;
         return block_hash;
     }
 
